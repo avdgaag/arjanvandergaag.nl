86864dc320f355addcaf2a31e8ce1970f43fa2f9
Arjan van der Gaag <info@agwebdesign.nl>
2011-03-23 09:59:54 +0100

    Moved sitemap and feed into preprocessor

--- lib/clean_sitemap_filter.rb
+++
@@ -1,6 +0,0 @@
-class CleanSitemapFilter < Nanoc3::Filter
-  identifier :clean_sitemap
-  def run(content, params = {})
-    content.gsub(/<url>\s*<loc>[^<]+?(txt|css|png|jpg|xml|js|404\.html|google[a-z0-9]+\.html|\.htaccess)<\/loc>\s*<lastmod>.+?<\/lastmod>\s*<\/url>\s*/m, '')
-  end
-end
