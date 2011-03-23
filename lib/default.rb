fad96936784cae00a4c8b05f4c0b1a86a94ef0eb
Arjan van der Gaag <info@agwebdesign.nl>
2011-03-23 10:00:24 +0100

    Better meta tags

--- lib/default.rb
+++ lib/default.rb
@@ -4,6 +4,25 @@ include Nanoc3::Helpers::LinkTo
 include Nanoc3::Helpers::Filtering
 include Nanoc3::Helpers::XMLSitemap
 
+# Output a meta-tag for use in your site header. The key you supply is looked
+# up in the configuration under 'meta_data'. You can override it on a per-item
+# basis.
+#
+# Usage:
+#
+#   <%= meta_tag :keywords %>
+#
+# This will output:
+#
+#   <meta name="keywords" value="...">
+#
+# Here, '...' is either the value of @item[:keywords] or that of
+# @site.config[:keywords].
+def meta_tag(key)
+  value = @item[key] || @site.config[:site][key]
+  '<meta name="%s" content="%s">' % [key, value] if value
+end
+
 def published_at(item)
   Time.parse(item[:created_at].to_s).strftime '%d-%m-%Y'
 end
