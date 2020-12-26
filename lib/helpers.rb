4d29c674c700db32a911da77b2fed980b3fad034
Arjan van der Gaag <arjan@arjanvandergaag.nl>
2020-12-26 23:47:16 +0100

    Parse Ulysses attributes

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -2,18 +2,13 @@ require 'rss'
 require 'polites/nanoc'
 use_helper Nanoc::Helpers::LinkTo
 
-def article_parts(content)
-  content = Nokogiri::HTML.fragment(content)
-  title = content.css('h1').first&.remove
-  leader = content.css('p').first&.remove
-  [title, leader, content]
-end
-
 # Filter for removing all HTML tags from some content.
 Nanoc::Filter.define(:drop_empty_paragraphs) do |content, _params|
   doc = Nokogiri::HTML.fragment(content)
   doc.children.each do |node|
     node.remove if node.name == 'p' && node.children.empty?
   end
+  doc.children.first(3).each(&:remove)
+  doc.css('p').first.append_class('leader')
   doc.to_html
 end
